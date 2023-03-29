0635989ab9cba88bd41b258926180771e5b59906
Josiane Silva <josianesouza8752@gmail.com>
2023-03-29 18:36:59 -0300

    No matchroute

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,22 @@
 
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter, Routes,Route, } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, } from 'react-router-dom';
 import App from './App';
 import Expenses from './routes/Expenses/expenses';
 import Invoices from './routes/Invoices/invoices';
+import NotFound from './routes/NotFound';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 
   <BrowserRouter>
-  <Routes>
-    <Route path="/" element={<App />}>
-    <Route path="expenses" element={<Expenses/>}/>
-    <Route path="invoices" element={<Invoices/>}/>
-    </Route>
-  </Routes>
-    
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="invoices" element={<Invoices />} />
+        <Route path="*" element={<NotFound />}
+        />
+      </Route>
+    </Routes>
+
   </BrowserRouter>
 )
