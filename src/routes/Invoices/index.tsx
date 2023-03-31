1ccd6c9646d718cefaaeb6e1ab1c8708c703397c
Josiane Silva <josianesouza8752@gmail.com>
2023-03-31 13:20:26 -0300

    Search Params

--- src/routes/Invoices/index.tsx
+++ src/routes/Invoices/index.tsx
@@ -1,10 +1,10 @@
 import "./styles.css";
-import { NavLink } from "react-router-dom";
-import {  Outlet } from "react-router-dom";
+import { NavLink, Outlet, useSearchParams } from "react-router-dom";
 import { getInvoices } from "../../Data/data";
 
 export default function Invoices() {
-  let invoices = getInvoices();
+  const invoices = getInvoices();
+  const [searchParams, setSearchParams] = useSearchParams();
   return (
     <div style={{ display: "flex" }}>
       <nav
@@ -13,15 +13,34 @@ export default function Invoices() {
           padding: "1rem",
         }}
       >
-        {invoices.map((invoices) => (
-          <NavLink
-            className={({isActive})=> isActive ? "dblock nav-red" : "dblock nav-blue"}
-            to={`/invoices/${invoices.number}`}
-            key={invoices.number}
-          >
-            {invoices.name}
-          </NavLink>
-        ))}
+        <input
+          value={searchParams.get("name") || ""}
+          onChange={(event) => {
+            let filter = event.target.value;
+            if (filter) {
+              setSearchParams({ filter });
+            } else {
+              setSearchParams({});
+            }
+          }}
+        />
+
+        {invoices
+          .filter((invoice) => {
+            const filter = searchParams.get("filter");
+            if (!filter) return true;
+            const name = invoice.name.toLowerCase();
+            return name.startsWith(filter.toLowerCase());
+          })
+          .map((invoices) => (
+            <NavLink
+              className={({ isActive }) => isActive ? "dblock nav-red" : "dblock nav-blue"}
+              to={`/invoices/${invoices.number}`}
+              key={invoices.number}
+            >
+              {invoices.name}
+            </NavLink>
+          ))}
       </nav>
       <Outlet />
     </div>
